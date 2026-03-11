c159775262a9620c7d924951aacff54d2f321b9d
Thiago <thiago@ISC.local>
2026-03-11 14:46:18 -0300

    ajuste, zerar pedding de root | implementacao, adicionado ao header botao com nome do usuario quando clicardo realizar o direcionamento para a pagina /signup

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,12 +2,20 @@ import { useState } from "react";
 import CardPost from "../../components/CardPost/CardPost";
 import api from "../../api/axios";
 import { useSelector } from "react-redux";
+import type { RootState } from "../../Redux/store";
+import { CgLogOut } from "react-icons/cg";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
-  const username = useSelector((state: any) => state.signupUsername.userName);
+  const username = useSelector((state: RootState) => state.signupUsername.userName);
+
+    const navigate = useNavigate();
+
+    const params = new URLSearchParams(window.location.search);
+    const userlogged = params.get("username");
 
   const handleTitleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
@@ -16,6 +24,10 @@ const Home = () => {
   const handleContentInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setContent(e.target.value);
   };
+
+  const handleLogout = () => {
+    navigate("/signup");
+  }
 
   const handleSubmitPost = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
@@ -39,8 +51,12 @@ const Home = () => {
 
   return (
     <>
-      <header className="bg-[#7695EC] w-[800px] h-16 flex items-center px-10 text-white text-2xl font-semibold">
+      <header className="bg-[#7695EC] w-[800px] h-16 flex items-center justify-between px-10 text-white text-2xl font-semibold">
         <h1>CodeLeap Network</h1>
+        <button onClick={() => handleLogout()} className="flex items-center gap-1 cursor-pointer hover:bg-[#6a8be6] p-1.5 rounded-[8px]">
+          <CgLogOut size={24} className="relative top-[2px]"/>
+          <h2 className="text-[20px]">{userlogged}</h2>
+        </button>
       </header>
       <main className="flex flex-col justify-center gap-5 p-5 bg-[#ffffff] w-[800px]">
         <form className="flex flex-col gap-5 border-1 border-[#99999999] rounded-[16px] color w-[752px] h-[334px] p-5 ">
